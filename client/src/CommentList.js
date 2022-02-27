71024c8973595580c6315fb1c5f522d80a6cf8a7
Raghav Kattel <arg.ahv7@gmail.com>
2022-02-27 15:28:59 +0545

    initial commit- services, deployments, skaffold api services

---
+++ client/src/CommentList.js
@@ -0,0 +1,20 @@
+const CommentList = ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
+    let content;
+
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+    if (comment.status === "pending") {
+      content = "Comment is awaiting moderation";
+    }
+    if (comment.status === "rejected") {
+      content = "Comment Rejected";
+    }
+    return <li key={comment.id}>{content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;
